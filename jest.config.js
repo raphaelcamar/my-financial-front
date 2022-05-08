125bc272edac5d189167238cb732c49ae1b70a2a
Raphael <raphaelcamar@outlook.com>
2022-05-08 18:31:00 -0300

    chore: adding react-testing-library

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
 module.exports = {
+  roots: ['<rootDir>/src'],
   verbose: true,
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
   testEnvironment: 'jsdom',
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/'
+  ],
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
-  }
+  },
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
 }
