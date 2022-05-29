d866e27ad6748874f37d4b79d2208f63559936d1
Raphael <raphaelcamar@outlook.com>
2022-05-29 17:34:32 -0300

    test: builderValidation test

---
+++ src/core/validation/builder/builder-validation.test.ts
@@ -0,0 +1,17 @@
+import { EmailValidation, RequiredFieldValidation, MaxLengthValidation, MinLengthValidation } from '@/core/data';
+import { ValidationBuilder } from '@/core/validation';
+
+describe('Builder validation', () => {
+  test('Should be able to make all signed validations', () => {
+    const field = 'test';
+
+    const validations = ValidationBuilder.field(field).email().maxLength(5).minLength(1).required().build();
+
+    expect(validations).toEqual([
+      new EmailValidation(field),
+      new MaxLengthValidation(field, 5),
+      new MinLengthValidation(field, 1),
+      new RequiredFieldValidation(field),
+    ]);
+  });
+});
