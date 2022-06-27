969dbb718e06a386a176a0bc76aef924ccd5b630
Raphael <raphaelcamar@outlook.com>
2022-06-27 19:59:22 -0300

    feat: paper component

--- src/core/presenters/components/atoms/paper/index.tsx
+++ src/core/presenters/components/atoms/paper/index.tsx
@@ -6,6 +6,12 @@ interface IPaper extends React.HTMLAttributes<HTMLDivElement> {
   density: 'lowShadow' | 'highShadow';
 }
 
+export type Density = 0 | 1 | 2;
+
+export interface IPpaperNew extends React.HTMLAttributes<HTMLDivElement> {
+  density: Density;
+}
+
 export const Paper: React.FC<IPaper> = ({ density, children, className, ...props }) => {
   const classes = useStyles();
   return (
--- src/core/presenters/components/atoms/paper/styles.ts
+++ src/core/presenters/components/atoms/paper/styles.ts
@@ -1,5 +1,7 @@
 import { Theme } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
+import styled, { css } from 'styled-components';
+import { Density, IPpaperNew } from '.';
 
 export const useStyles = makeStyles(
   (theme: Theme) => ({
@@ -18,3 +20,22 @@ export const useStyles = makeStyles(
   }),
   { name: 'PaperComponent' }
 );
+
+const getShadowDensity = (density: Density) =>
+  ({
+    0: css`
+      box-shadow: none;
+    `,
+    1: css`
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    `,
+    2: css`
+      box-shadow: 0px 4px 10px 4px rgba(0, 0, 0, 0.25);
+    `,
+  }[density]);
+
+export const PaperContainer = styled.div<IPpaperNew>`
+  border-radius: 12;
+  padding: 16px 12px;
+  ${({ density }) => getShadowDensity(density)}
+`;
