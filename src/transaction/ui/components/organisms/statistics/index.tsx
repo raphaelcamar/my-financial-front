a1f5699f7512a4f2ed343e9a97a3ec02a0b4f6be
Raphael <raphaelcamar@outlook.com>
2022-07-18 21:41:47 -0300

    feat: loading in statistics

--- src/transaction/ui/components/organisms/statistics/index.tsx
+++ src/transaction/ui/components/organisms/statistics/index.tsx
@@ -1,14 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CardStatistic } from '@/transaction/ui/components/molecules';
 import { Container } from './styles';
 import { useTransactionContext } from '@/transaction/presenters/contexts';
+import { WrapperLoader } from '@/core/ui/components/molecules';
+import { delay } from '@/core/utils';
 
 export const Statistics: React.FC = () => {
   const { filter, statistic, getStatisticsByFilter } = useTransactionContext();
+  const [loading, setLoading] = useState<boolean>(false);
 
   const getStatistics = async () => {
     if (filter) {
-      await getStatisticsByFilter(filter);
+      try {
+        setLoading(true);
+        await delay(15000);
+        await getStatisticsByFilter(filter);
+      } finally {
+        setLoading(false);
+      }
     }
   };
 
@@ -17,33 +26,36 @@ export const Statistics: React.FC = () => {
   }, [filter]);
 
   return (
-    <Container>
-      <CardStatistic
-        title="Total de gastos"
-        description={`${statistic?.totalFilter?.filter?.start} à ${statistic?.totalFilter?.filter?.limit}`}
-        price={statistic?.totalFilter?.value}
-        icon="recurrencyValue"
-      />
-      <CardStatistic
-        title="Maior gasto"
-        description={statistic?.mostSpent?.description}
-        price={statistic?.mostSpent?.value}
-        icon="arrowLimit"
-      />
-      <CardStatistic
-        title="Média de gasto"
-        description={`${statistic?.averageSpent?.filter?.start} à ${statistic?.averageSpent?.filter?.limit}`}
-        price={statistic?.averageSpent?.value}
-        icon="balance"
-      />
-      <CardStatistic
-        title="Total da carteira"
-        description="Total de todas as transações"
-        price={statistic?.total}
-        icon="moneyBag"
-        allowNegative
-        isNegative={statistic?.total < 0}
-      />
-    </Container>
+    <WrapperLoader loading={loading} size={15} color="primary">
+      <Container>
+        <CardStatistic
+          title="Total de gastos"
+          description={`${statistic?.totalFilter?.filter?.start} à ${statistic?.totalFilter?.filter?.limit}`}
+          price={statistic?.totalFilter?.value}
+          icon="recurrencyValue"
+        />
+
+        <CardStatistic
+          title="Maior gasto"
+          description={statistic?.mostSpent?.description}
+          price={statistic?.mostSpent?.value}
+          icon="arrowLimit"
+        />
+        <CardStatistic
+          title="Média de gasto"
+          description={`${statistic?.averageSpent?.filter?.start} à ${statistic?.averageSpent?.filter?.limit}`}
+          price={statistic?.averageSpent?.value}
+          icon="balance"
+        />
+        <CardStatistic
+          title="Total da carteira"
+          description="Total de todas as transações"
+          price={statistic?.total}
+          icon="moneyBag"
+          allowNegative
+          isNegative={statistic?.total < 0}
+        />
+      </Container>
+    </WrapperLoader>
   );
 };
