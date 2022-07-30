f724ba212e1fc5c92f44de0ff8db8f21ee1be07c
Raphael <raphaelcamar@outlook.com>
2022-07-30 10:32:22 -0300

    refactor: profile components

--- src/profile/ui/pages/profile-page/index.tsx
+++ src/profile/ui/pages/profile-page/index.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import { BackgroundCardProfile } from '@/profile/ui/components/atoms';
 import { Container } from './styles';
-import { useAccessAndAuthContext } from '@/access-and-auth/presenters';
-import { BodyInformationCard } from '@/profile/ui/components/molecules';
+import { ContainerProfile } from '@/profile/ui/components/organisms';
 
-export const ProfilePage: React.FC = () => {
-  const { user } = useAccessAndAuthContext();
-
-  return (
-    <Container>
-      <div>
-        <BackgroundCardProfile />
-        <BodyInformationCard user={user} />
-      </div>
-    </Container>
-  );
-};
+export const ProfilePage: React.FC = () => (
+  <Container>
+    <ContainerProfile />
+  </Container>
+);
