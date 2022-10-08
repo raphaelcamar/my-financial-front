469694f4773acafd3e1f0d9620fedc7d88a19baf
Raphael <raphaelcamar@outlook.com>
2022-10-08 14:12:39 -0300

    chore: removing uuid lib since inconsistency with jest

--- src/core/utils/random-id.ts
+++ src/core/utils/random-id.ts
@@ -1,3 +1,10 @@
-import { v4 as uuid } from 'uuid';
-
-export const randomId = (): string => uuid();
+export const randomId = (): string => {
+  const a = new Uint32Array(3);
+  window.crypto.getRandomValues(a);
+  return (
+    performance.now().toString(36) +
+    Array.from(a)
+      .map(A => A.toString(36))
+      .join('')
+  ).replace(/./g, `${Math.random()}${Intl.DateTimeFormat().resolvedOptions().timeZone}${Date.now()}`);
+};
