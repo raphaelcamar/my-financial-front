7a980648a4454a66e4d29faad7f7efe5e1ac0b3d
Raphael <raphaelcamar@outlook.com>
2022-10-09 00:12:37 -0300

    test: format currency

---
+++ src/core/utils/formatters/format-currency/format-currency.test.ts
@@ -0,0 +1,32 @@
+import { formatCurrency } from './format-currency';
+
+describe('FormatCurrency - Unit Test', () => {
+  test('Should be able to format the currency for PT_BR', () => {
+    const VALUE = 100;
+
+    const formattedValue = formatCurrency(VALUE);
+
+    expect(formattedValue).toContain('R$');
+    expect(formattedValue).toContain(',');
+  });
+
+  test('Should be able to format the currency for positive value in PT_BR ', () => {
+    const VALUE = -100;
+
+    const formattedValue = formatCurrency(VALUE, false);
+
+    expect(formattedValue).toContain('R$');
+    expect(formattedValue).toContain(',');
+    expect(formattedValue).not.toContain('-');
+  });
+
+  test('Should be able to format the currency for negative value in PT_BR ', () => {
+    const VALUE = -100;
+
+    const formattedValue = formatCurrency(VALUE, true);
+
+    expect(formattedValue).toContain('R$');
+    expect(formattedValue).toContain(',');
+    expect(formattedValue).toContain('-');
+  });
+});
