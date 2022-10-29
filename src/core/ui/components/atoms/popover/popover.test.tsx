510e1a3fb80306091ca19f4661a79ebfc91beecd
Raphael <raphaelcamar@outlook.com>
2022-10-29 12:25:32 -0300

    test: popover tests

--- src/core/ui/components/atoms/popover/popover.test.tsx
+++ src/core/ui/components/atoms/popover/popover.test.tsx
@@ -9,19 +9,30 @@ describe('Popover - Unit test', () => {
   test('Should be able to render the popover element, with all attributes', () => {
     render(
       ProvideStyledTheme(
-        <Popover displayElement={<div data-testid="some-element">Some text</div>}>
-          <section data-testid="content">Some content</section>
-        </Popover>
+        <div data-testid="div-to-click">
+          <Popover displayElement={<div data-testid="some-element">Some text</div>}>
+            <section data-testid="content">Some content</section>
+          </Popover>
+        </div>
       )
     );
 
     const popover = screen.getByTestId('menu') as HTMLDivElement;
+    const div = screen.getByTestId('div-to-click') as HTMLDivElement;
     const displayElement = screen.getByTestId('some-element') as HTMLDivElement;
     const content = screen.getByTestId('content');
 
+    fireEvent(
+      div,
+      new MouseEvent('click', {
+        cancelable: false,
+        bubbles: true,
+      })
+    );
+
     expect(popover).toBeInTheDocument();
     expect(displayElement).toBeInTheDocument();
-    expect(content).not.toBeInTheDocument();
+    expect(content).not.toBeVisible();
 
     fireEvent(
       popover,
