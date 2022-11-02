ba67a1ca56593c0729b4721432c81a6c5039d8df
Raphael <raphaelcamar@outlook.com>
2022-11-02 16:41:42 -0300

    feat: redirecting not found to login if not logged

--- src/core/ui/components/molecules/not-found/index.tsx
+++ src/core/ui/components/molecules/not-found/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { TextLink, Typography } from '@/core/ui/components/atoms';
 import { Container, Content, StyledTypography } from './styles';
+import { useAccessContext } from '@/user/presenters';
 
-export const NotFound: React.FC = () => (
-  <Container data-testid="not-found-page">
-    <Content>
-      <StyledTypography size="xxxlarge" color="primary" weight={600}>
-        404
-      </StyledTypography>
-      <div style={{ display: 'flex' }}>
-        <Typography weight={400}>Página não encontrada!</Typography>
-        <TextLink to="/transacoes">Volte para tela inicial</TextLink>
-      </div>
-    </Content>
-  </Container>
-);
+export const NotFound: React.FC = () => {
+  const { verifyInCache } = useAccessContext();
+  return (
+    <Container data-testid="not-found-page">
+      <Content>
+        <StyledTypography size="xxxlarge" color="primary" weight={600}>
+          404
+        </StyledTypography>
+        <div>
+          <Typography weight={400}>Página não encontrada!</Typography>
+          <TextLink to={!verifyInCache() ? '/login' : '/transacoes'}>Volte para tela inicial</TextLink>
+        </div>
+      </Content>
+    </Container>
+  );
+};
