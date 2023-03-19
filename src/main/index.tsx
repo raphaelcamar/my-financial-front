79e968332d66ecffb0b6f614d68ae97a5a832207
Raphael Camargo <raphaelcamar@outlook.com>
2023-03-19 18:43:24 -0300

    feat: ui-lib

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,6 @@
-// import 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './global.css';
-
 import { Root } from './root';
 
 // if (process.env.NODE_ENV === 'development') {
@@ -10,11 +8,3 @@ import { Root } from './root';
 // }
 
 ReactDOM.render(<Root />, document.getElementById('main'));
-
-if (import.meta.webpackHot.hot) {
-  console.log('Entrou aqui');
-  import.meta.webpackHot.hot.accept('./root.tsx', () => {
-    const NewRoot = require('./root.tsx').default;
-    ReactDOM.render(<NewRoot />, document.getElementById('main'));
-  });
-}
--- src/user/ui/pages/login/index.tsx
+++ src/user/ui/pages/login/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from '@raphaelcamar/ui-lib';
 import { LoginForm } from '@/user/ui/components/organisms';
 import { WrapperFigureForm } from '@/user/ui/components/molecules';
 
 export const Login: React.FC = () => (
   <WrapperFigureForm side="left" figureType="circle" testId="login-page">
     <LoginForm />
+    <Button>TResrt</Button>
   </WrapperFigureForm>
 );
 
